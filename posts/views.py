c20a7e620c50bdf71e4a6d0bf417815db61ccb92
Lolin <akinshin2022@list.ru>
2022-12-21 16:37:39 +0300

    add auth

--- posts/views.py
+++ posts/views.py
@@ -4,16 +4,18 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .form import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 #Здесь мы прописываем представления для шаблонов
 def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.all()
     return render(request, 'posts/post_list.html', {'posts':posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
-
+@login_required
 def post_new(request):
     #Если у нас пост запрос, то мы добавляем
     #наш новый пост в бд
@@ -29,7 +31,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'posts/post_edit.html', {'form': form})
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
